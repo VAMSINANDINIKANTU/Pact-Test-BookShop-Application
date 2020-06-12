f9ffd29973208ca1c974086fff2946347a1125c3
VAMSINANDINIKANTU <56575896+VAMSINANDINIKANTU@users.noreply.github.com>
2020-06-12 17:00:15 +0530

    first commit

--- PactDemo/ApiConsumer-1/src/main/java/com/bookshopapp/exceptionhandler/InvalidExceptionHandler.java
+++ PactDemo/ApiConsumer-1/src/main/java/com/bookshopapp/exceptionhandler/InvalidExceptionHandler.java
@@ -8,11 +8,9 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.WebRequest;
-
 @ControllerAdvice
 @RestController
 public class InvalidExceptionHandler {
-	
 	@ExceptionHandler(Exception.class) 
 	public ResponseEntity<ErrorResponse>handleOtherExceptions(Exception ex,WebRequest request){
 		  List<String> errors = new ArrayList<>();
