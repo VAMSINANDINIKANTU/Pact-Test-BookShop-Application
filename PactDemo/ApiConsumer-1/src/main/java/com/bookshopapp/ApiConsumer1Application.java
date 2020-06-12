66aa069131cf435b2b16528a4e9b0605b0620e65
VAMSINANDINIKANTU <56575896+VAMSINANDINIKANTU@users.noreply.github.com>
2020-06-12 17:01:22 +0530

    first commit

--- PactDemo/ApiConsumer-1/src/main/java/com/bookshopapp/ApiConsumer1Application.java
+++ PactDemo/ApiConsumer-1/src/main/java/com/bookshopapp/ApiConsumer1Application.java
@@ -1,13 +1,9 @@
 package com.bookshopapp;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class ApiConsumer1Application {
-
-	public static void main(String[] args) {
+        public static void main(String[] args) {
 		SpringApplication.run(ApiConsumer1Application.class, args);
 	}
-
 }
