bb381e358750cd517eb8e61d795c0d10e5187559
VAMSINANDINIKANTU <56575896+VAMSINANDINIKANTU@users.noreply.github.com>
2020-06-12 16:48:08 +0530

    first comit

--- PactDemo/ApiConsumer-1/src/main/java/com/bookshopapp/controller/BookConsumerController.java
+++ PactDemo/ApiConsumer-1/src/main/java/com/bookshopapp/controller/BookConsumerController.java
@@ -1,8 +1,6 @@
 package com.bookshopapp.controller;
-
 import java.io.IOException;
 import java.util.Set;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,30 +9,27 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.bookshopapp.entities.Book;
 import com.bookshopapp.exceptionhandler.BookNotFoundException;
 import com.bookshopapp.service.BookConsumerService;
 
 @RestController
 public class BookConsumerController {
-	@Autowired
-	BookConsumerService bookConsumerService;
-	 @GetMapping("hii")
-	    public ResponseEntity<String> keepAlive() {
-	        return ResponseEntity.ok("hii");
-	    }
+       @Autowired
+       BookConsumerService bookConsumerService;
+       @GetMapping("hii")
+       public ResponseEntity<String> keepAlive() {
+	    return ResponseEntity.ok("hii");
+       }
 
-	@GetMapping("book/{id}")
-	public ResponseEntity<Book> getBook(@PathVariable("id") Long id) {
-		return bookConsumerService.getBook(id).map(ResponseEntity::ok).orElseThrow(() -> new BookNotFoundException(id));
-
-		
-	}
-	@GetMapping("bookName/{bookName}")
-	public ResponseEntity<Book> getBook(@PathVariable("bookName") String bookName) {
-		return bookConsumerService.getBook(bookName).map(ResponseEntity::ok).orElseThrow(() -> new BookNotFoundException(bookName));
-
-	}
+       @GetMapping("book/{id}")
+       public ResponseEntity<Book> getBook(@PathVariable("id") Long id) {
+	    return bookConsumerService.getBook(id).map(ResponseEntity::ok).orElseThrow(() -> new BookNotFoundException(id));
+       }
 	
+       @GetMapping("bookName/{bookName}")
+       public ResponseEntity<Book> getBook(@PathVariable("bookName") String bookName) {
+	    return bookConsumerService.getBook(bookName).map(ResponseEntity::ok).orElseThrow(() -> new BookNotFoundException(bookName));
+
+       }
 }
